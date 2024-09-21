42d2a4427219047f39137e258eab8456f0851436
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-09-21 20:24:38 +0200

    Update script to autoinclude common files (#54)
    Includes the following files:
    - pack.png
    - README.md
    - LICENSE

--- build.sh
+++ build.sh
@@ -6,11 +6,17 @@ mkdir target/
 for item in *; do
     if [ -d "$item" ]; then
         if [ "$item" != "." ] && [ "$item" != ".." ] && [ "$item" != "pics" ] && [ "$item" != "target" ]; then
-            cd "$item"
-            name="Vanitas-${1}-${item}.zip"
-            echo "$name"
-            zip -r -q -0 "$name" *
-            cp "$name" ../target/
+            dir="${item}/"
+            file="Vanitas-${1}-${item}.zip"
+            echo "Compiling ${file}"
+            cp "LICENSE" "$dir"
+            cp "pack.png" "$dir"
+            cp "README.md" "$dir"
+            cd "$dir"
+            zip -r -q -0 "$file" *
+            cp "$file" ../target/
+            rm "$file"
+            rm "LICENSE" "pack.png" "README.md"
             cd ..
         fi
     fi
