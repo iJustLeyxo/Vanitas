dca2c73c50c316882e29200f6db1b5ef2baac87e
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-12-02 11:39:18 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -1,16 +1,38 @@
 #bin/bash
 
-echo "Downloading resourcepack compiler"
+# Resourcepack compiler
+
 rm -rf PackMake.jar PackMake.zip PackMake/
-curl -L -o "PackMake.zip" "https://api.github.com/repos/iJustLeyxo/PackMake/releases/latest"
-unzip PackMake.zip
-cd PackMake
+
+PM_VER=$(echo $(curl -s https://api.github.com/repos/iJustLeyxo/PackMake/releases/latest) | jq -r '.tag_name')
+PM_URL="https://github.com/iJustLeyxo/PackMake/archive/refs/tags/$PM_VER.zip"
+
+if [ -z "$PM_URL" ]; then
+    echo "Faulty download url"
+    exit 1
+fi
+
+echo "Downloading resourcepack compiler from $PM_URL"
+curl -sLo "PackMake.zip" $PM_URL
+
+echo "Extracting resourcepack compiler"
+unzip -q PackMake.zip
+rm -f PackMake.zip
+
+PM_DIR="PackMake-$PM_VER/"
+cd "$PM_DIR"
+
 echo "Compiling resourcepack compiler"
-mvn clean package
+mvn -q clean package
 cd ..
-cp PackMake/target/PackMake.jar .
+cp "$PM_DIR/target/PackMake.jar" "PackMake.jar"
+rm -rf "$PM_DIR"
+
+echo "Compiling resourcepack"
 java -jar PackMake.jar
-rm -rf PackMake.jar PackMake.zip PackMake/
+rm -f PackMake.jar
+
+# Resourcepack packaging
 
 cd target/
 
@@ -19,14 +41,14 @@ for item in *; do
         if [ "$item" != "." ] && [ "$item" != ".." ]; then
             dir="${item}/"
             file="Vanitas-${1}-${item}.zip"
-            echo "Compiling ${file}"
+            echo "Packaging ${file}"
             cp "../LICENSE" "$dir"
             cp "../README.md" "$dir"
             cd "$dir"
-            zip -r -q -0 "$file" *
-            cp "$file"
-            rm -rf "$dir"
+            zip -rq0 "$file" *
+            cp "$file" "../$file"
             cd ..
+            rm -rf "$dir"
         fi
     fi
 done
