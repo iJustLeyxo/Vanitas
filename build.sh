79d06b8635a2f7acbd0a76424764fe4a81348d98
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-12-02 10:58:14 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,22 +1,31 @@
 #bin/bash
 
-rm -rf target/
-mkdir target/
+echo "Downloading resourcepack compiler"
+rm -rf PackMake.jar PackMake.zip PackMake/
+curl -L -o "PackMake.zip" "https://api.github.com/repos/iJustLeyxo/PackMake/releases/latest"
+unzip PackMake.zip
+cd PackMake
+echo "Compiling resourcepack compiler"
+mvn clean package
+cd ..
+cp PackMake/target/PackMake.jar .
+java -jar PackMake.jar
+rm -rf PackMake.jar PackMake.zip PackMake/
+
+cd target/
 
 for item in *; do
     if [ -d "$item" ]; then
-        if [ "$item" != "." ] && [ "$item" != ".." ] && [ "$item" != "pics" ] && [ "$item" != "target" ] && [ "$item" != "tools" ]; then
+        if [ "$item" != "." ] && [ "$item" != ".." ]; then
             dir="${item}/"
             file="Vanitas-${1}-${item}.zip"
             echo "Compiling ${file}"
-            cp "LICENSE" "$dir"
-            cp "pack.png" "$dir"
-            cp "README.md" "$dir"
+            cp "../LICENSE" "$dir"
+            cp "../README.md" "$dir"
             cd "$dir"
             zip -r -q -0 "$file" *
-            cp "$file" ../target/
-            rm "$file"
-            rm "LICENSE" "pack.png" "README.md"
+            cp "$file"
+            rm -rf "$dir"
             cd ..
         fi
     fi
