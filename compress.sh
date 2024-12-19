e6c193c1ec8bd3a5afbc95b9e18fb0b860b5f18c
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-12-19 21:16:53 +0100

    Update PackMake Version (#113)
    * Delete obsolete compression script
    * Add new PackMake options
    * Update build script PackMake version
    * ReCompress (#112)

--- compress.sh
+++
@@ -1,4 +0,0 @@
-#bin/bash
-# Requires optipng
-
-find . \( -path ./.git -prune -o -path ./target -prune \) -o -type d -exec bash -c 'echo "Compressing $0" && cd $0 && optipng -silent -strip all -o7 *png' {} \;
